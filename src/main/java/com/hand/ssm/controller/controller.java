8e39233df0a6af87b4b85e435ea045ff44015d6c
hand <hand@hand-PC>
2016-04-05 21:22:00 +0800

    commit

---
+++ src/main/java/com/hand/ssm/controller/controller.java
@@ -0,0 +1,13 @@
+package com.hand.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class controller {
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public String hello() {
+		return "hello";
+	}
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2 style="text-align:center">Hello World</h2>
+<a href="hello">hello</a>
+</body>
+</html>
