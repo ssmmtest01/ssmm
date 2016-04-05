f89e379800718fe42d8adf44cb017924de6d2f4f
liwenzlw <2462077343@qq.com>
2016-04-05 16:35:31 +0800

    结束测试

--- src/main/java/com/hand/controlers/MvcControler.java
+++ src/main/java/com/hand/controlers/MvcControler.java
@@ -1,18 +1,40 @@
 package com.hand.controlers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MvcControler {
 
 	@ResponseBody
-	@RequestMapping(value="mvc1",method=RequestMethod.GET)
-	public String mvc1(@RequestParam String data){
+	@RequestMapping(value = "mvc1", method = RequestMethod.GET)
+	public String mvc1(@RequestParam String data) {
 		System.out.println(data);
 		return data;
+	}
+
+	@RequestMapping(value = "testRedirect/{name}/{age}")
+	public String testRedirect(@PathVariable String name, @PathVariable int age) {
+
+		System.out.println("name:" + name + "  age:" + age);
+		System.out.println("这是重定向，通知浏览器访问hello.jsp，数据带不过去");
+		return "redirect:/redirectTest.jsp";
+	}
+	
+	@RequestMapping(value = "testForward/{name}/{age}")
+	public String testForward(@PathVariable String name, @PathVariable int age,ModelAndView mv ) {
+		
+		System.out.println("name:" + name + "  age:" + age);
+		System.out.println("这是转发，通知服务器访问/WEB-INF/views/forwardTest.jsp，数据通过mv带过去");
+		mv.addObject("name", name);
+		mv.addObject("age", age);
+		return "forward:/WEB-INF/views/forwardTest.jsp";
 	}
 }
---
+++ src/main/webapp/MainTest.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>测试主界面</title>
+</head>
+<body>
+	<h2>测试重定向</h2><br/>
+	<a href="testRedirect/小红/18">testRedirect/小红/18</a>
+	<hr/>
+	<h2>测试转发</h2><br/>
+	<a href="testForward/小红/18">testRedirect/小红/18</a>
+	<hr/>
+</body>
+</html>
