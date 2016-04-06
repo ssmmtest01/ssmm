5bd85d6ae1fca9a5a4aec815c10ed6401b9c7d71
Aloecho <284342817@qq.com>
2016-04-06 16:06:46 +0800

    commit

--- src/main/java/com/hand/ssm/controller/HelloController.java
+++ src/main/java/com/hand/ssm/controller/HelloController.java
@@ -8,10 +8,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+	
+	/*返回视图模型*/
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public ModelAndView hello() {
 		ModelAndView mv = new ModelAndView();
@@ -20,6 +24,7 @@ public class HelloController {
 		return mv;
 	}
 	
+	/*转发*/
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String welcome(HttpServletRequest request ,HttpServletResponse response) {
 		 response.setCharacterEncoding("UTF-8");
@@ -27,4 +32,21 @@ public class HelloController {
 		return "welcome";
 	}
 	
+	/*参数绑定*/
+	@RequestMapping(value = "/param", method = RequestMethod.GET)
+	public ModelAndView TestParam(@RequestParam int id) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("id", id);
+		mv.setViewName("param");
+		return mv;
+	}	
+		
+	/*rest、ant、正则表达式风格*/
+	@RequestMapping(value ={ "/{id}/rest", "/{id}/rest*", "{id}/{categoryCode:\\d+}-{pageNumber:\\d+}"}, method = RequestMethod.GET)
+	public ModelAndView TestRest(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("id", id);
+		mv.setViewName("param");
+		return mv;
+	}	
 }
---
+++ src/main/webapp/readme.txt
@@ -0,0 +1,2 @@
+http://localhost:8080/hello.do
+http://localhost:8080/welcome.do
