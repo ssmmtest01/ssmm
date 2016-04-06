5dff11d5fe2149fcf70f2b70aae084eb2b488d17
hetaoo <i@hetaoo.com>
2016-04-06 16:32:12 +0800

    添加httpinvoker

---
+++ httpinvoker_server/src/main/java/org/felix/service/UserService.java
@@ -0,0 +1,7 @@
+package org.felix.service;
+
+import org.felix.model.User;
+
+public interface UserService {
+	void add(User u);
+}
---
+++ httpinvoker_server/src/main/java/org/felix/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package org.felix.service.impl;
+
+import org.felix.model.User;
+import org.felix.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void add(User u) {
+		System.out.println("add user[" + u.getName() + "]");
+		System.out.println("this server data");
+	}
+
+}
