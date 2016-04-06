a20c0445281358d7b966d4c32b6197ca34fdf58a
yeke8888 <1390578952@qq.com>
2016-04-06 20:07:40 +0800

    add httpinvoker

---
+++ httpinvoker_client/src/main/java/org/felix/service/UserService.java
@@ -0,0 +1,5 @@
+package org.felix.service;
+import org.felix.model.User;
+public interface UserService {
+    void add(User u);
+}
---
+++ httpinvoker_server/src/main/java/org/felix/service/UserService.java
@@ -0,0 +1,6 @@
+package org.felix.service;
+import org.felix.model.User;
+public interface UserService {
+	void add(User u);
+
+}
