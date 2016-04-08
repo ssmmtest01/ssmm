ec42ce4790030694751e1a24bd620b738debf7b0
zhangyongdong <2474576148@qq.com>
2016-04-08 15:23:23 +0800

    更新。。。

---
+++ src/main/java/sy/interceptor/MyInterceptor.java
@@ -0,0 +1,34 @@
+package sy.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class MyInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("preHandle毕业论文怎么办     MyInterceptor  1号拦截器");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("postHandle毕业论文有办法？     MyInterceptor  1号拦截器    ");
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("afterCompletion毕业论文很好解决的嘛，so easy！     MyInterceptor  1号拦截器");
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+}
